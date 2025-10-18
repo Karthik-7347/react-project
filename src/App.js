13e29485013a7026b0b798d30ca3f4105ea29e1f
Karthik-7347 <Karthikvs7347@gmail.com>
2025-10-18 11:37:06 +0530

    Update CSS App Component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div className='container'>
-      <h2>React Default Component Page</h2>
+      <h2 className='text-primary'>React Default Component Page</h2>
       
     </div>
   )
